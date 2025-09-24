6c2945b6cd4989adc56491836b19956fa5c514c0
Apoorva Dwivedi <apoorva.24scse1180500@galgotiasuniversity.ac.in>
2025-09-24 20:45:39 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,10 @@
+class FI {
+    static int f(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return f(n - 1) + f(n - 2);
+    }
+    public static void main(String[] args) {
+        System.out.println(f(6));
+    }
+}
